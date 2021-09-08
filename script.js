b5779b3b781fa7f5b3eddc0febd608d5e3b651b3
emilievdweele <74767348+emilievdweele@users.noreply.github.com>
2021-09-08 10:51:05 +0000

    balletje stuitert

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 var x, y, v;
 
 function setup() {
-  createCanvas (400, 300);
+  createCanvas (400, 400);
 
   x = 0;
   y = 200;
-  v = 5;
+  vx = 5;
+  vy = 5;
 }
 
 function draw() {
   background(225);
 
   ellipse(x,y,50,50)
-  x = x + v
+  x = x + vx
+  y = y + vy
 
-  if(x <= 0 || x > 400)
-  v = v * -1;
+  if(x <= 0 || x > 400) {
+    vx = vx * -1;
+  }
+
+  if (y <= 0 || y > 400){
+    vy = vy * -1;
+  }
 }
