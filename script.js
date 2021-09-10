879578fe2441a99200853921c64b1f949aa375c9
emilievdweele <74767348+emilievdweele@users.noreply.github.com>
2021-09-10 08:46:07 +0000

    3 stuiterende balletjes - ingewikkelde manier

--- script.js
+++ script.js
@@ -1,26 +1,65 @@
-var x, y, v;
+var x, m, q, y, n, r, v;
 
 function setup() {
-  createCanvas (400, 400);
+  createCanvas (500, 400);
 
   x = 0;
   y = 200;
+  m = 200;
+  n = 0;
+  q = 200;
+  r = 0;
   vx = 5;
   vy = 5;
+  vm = 7;
+  vn = 7;
+  vq = 8;
+  vr = 8;
 }
 
 function draw() {
+
   background(225);
 
   ellipse(x,y,50,50)
   x = x + vx
   y = y + vy
 
-  if(x <= 0 || x > 400) {
+  fill ('#fae');
+
+  if(x <= 0 || x > 500) {
     vx = vx * -1;
   }
 
   if (y <= 0 || y > 400){
     vy = vy * -1;
+  }
+
+  ellipse(m,n,50,50)
+  m = m + vm
+  n = n + vn
+
+  fill (255, 204, 100);
+
+  if(m <= 0 || m > 500) {
+    vm = vm * -1;
+  }
+
+  if (n <= 0 || n > 400){
+    vn = vn * -1;
+  }
+
+  ellipse(q,r,50,50)
+  q = q + vq
+  r = r + vr
+
+  fill ('rgba(100%,0%,100%,0.5)');
+
+  if(q <= 0 || q > 500) {
+    vq = vq * -1;
+  }
+
+  if (r <= 0 || r > 400){
+    vr = vr * -1;
   }
 }
