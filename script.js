e4094e77fdf57549d0bdce6a1367513e4246ed6f
emilievdweele <74767348+emilievdweele@users.noreply.github.com>
2021-09-15 10:49:49 +0000

    class ball

--- script.js
+++ script.js
@@ -1,90 +1,39 @@
-var x, m, q, y, n, r, v;
-
-function setup() {
-  createCanvas (500, 400);
-
-  x = 0;
-  y = 200;
-  m = 200;
-  n = 0;
-  q = 200;
-  r = 0;
-  vx = 5;
-  vy = 5;
-  vm = 7;
-  vn = 7;
-  vq = 8;
-  vr = 8;
-}
-
-function draw() {
-
-  background(225);
-
-  ellipse(x,y,50,50)
-  x = x + vx
-  y = y + vy
-
-  fill ('#fae');
-
-  if(x <= 0 || x > 500) {
-    vx = vx * -1;
-  }
-
-  if (y <= 0 || y > 400){
-    vy = vy * -1;
-  }
-
-  ellipse(m,n,50,50)
-  m = m + vm
-  n = n + vn
-
-  fill (255, 204, 100);
-
-  if(m <= 0 || m > 500) {
-    vm = vm * -1;
-  }
-
-  if (n <= 0 || n > 400){
-    vn = vn * -1;
-  }
-
-  ellipse(q,r,50,50)
-  q = q + vq
-  r = r + vr
-
-  fill ('rgba(100%,0%,100%,0.5)');
-
-  if(q <= 0 || q > 500) {
-    vq = vq * -1;
-  }
-
-  if (r <= 0 || r > 400){
-    vr = vr * -1;
-  }
-}
-
 class Ball {
-  constructor(x, y, w, h, vx, vy){
+  constructor(x, y, w, h, vx, vy, color){
     this.x = x;
     this.y = y;
     this.w = w;
     this.h = h;
     this.vx = vx;
     this.vy = vy;
+    this.color = color;
   }
 
   drawBall(){
-    ellipse(this.x,this.y,this.w,this.h);
-    this.x = x + vx;
-    this.y = y + vy;
-    
-    if(this.x <= 0 || this.x > 500) {
-      this.vx = vx * -1;
+    ellipse (this.x, this.y, this.w, this.h, this.color);
+    this.x = this.x + this.vx;
+    this.y = this.y + this.vy;
+
+    if (this.x < 0 || this.x > 500) {
+      this.vx = this.vx * -1
     }
 
-    if (this.y <= 0 || this.y > 400){
-      this.vy = vy * -1;
+   if (this.y < 0 || this.y > 400) {
+      this.vy = this.vy * -1
     }
   }
+}
+
+function setup() {
+  createCanvas (500,400);
+  
+  ball1 = new Ball(10,10,50,50,5,5,"red");
+  ball2 = new Ball(50,50,40,40,7,7,"blue");
+}
+
+function draw() {
+    background(225);
+    
+    ball1.drawBall();
+    ball2.drawBall();
 }
